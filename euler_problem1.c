ca2ae3cc71ae12980566da468cd2857c6f69617d
Jakub Liu (QBA LIU) <113781664+JakubLiu@users.noreply.github.com>
2023-04-25 14:35:56 +0200

    Add files via upload

---
+++ euler_problem1.c
@@ -0,0 +1,30 @@
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+#include <stdio.h>
+int main() {
+int sum;
+float k;
+int n;
+
+sum = 0;
+
+for (k = 0, n = 0; k < 1000, n < 1000; ++k, ++n)
+{
+	if (k/3 == n/3 || k/5 == n/5)
+	{
+	
+		sum = sum + n;
+		// sum = sum + k;
+	
+	}
+
+}
+
+printf("sum = %d\n", sum);
+return 0;
+}
